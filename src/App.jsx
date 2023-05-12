69058f837fe36572b620d5120cc1d225c7efe889
KYEONGMIN CHO <ggingmin@gmail.com>
2023-05-12 15:09:42 +0900

    GitHub Actions workflows edited

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <p>클라우트타입 웨비나 #01</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
